874446565416bd2ffbc25f356254b92ce5dd7c17
Rabbit White <165736101+herogaso1@users.noreply.github.com>
2025-06-29 18:24:10 +0700

    aa

--- cv/views.py
+++ cv/views.py
@@ -4,7 +4,6 @@ from core.models import Account
 
 @login_required
 def cv_edit(request):
-    # Lấy dữ liệu từ tài khoản hiện tại nếu đã đăng nhập
     try:
         account = Account.objects.get(username=request.user.username)
         context = {
@@ -20,6 +19,24 @@ def cv_edit(request):
         context = {}
 
     return render(request, 'cv/cv.html', context)
+
+@login_required
+def cvsv_edit(request):
+    try:
+        account = Account.objects.get(username=request.user.username)
+        context = {
+            'email': account.email or '',
+            'first_name': account.first_name or '',
+            'last_name': account.last_name or '',
+            'address': account.company or '',  # Sử dụng đúng trường address cho sinh viên
+            'title': account.title or '',
+            'timezone': account.timezone or '',
+            'phone_number': account.phone_number or '',
+        }
+    except Account.DoesNotExist:
+        context = {}
+
+    return render(request, 'cv/cvsv.html', context)
 
 @login_required
 def password_2fa(request):
