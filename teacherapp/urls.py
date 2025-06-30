0c1ad10ba61fd1e6cb308b4191ce45eff122e2c1
Nghia1245654 <kingwinlata@gmail.com>
2025-06-30 09:11:04 +0700

    lam xong quan ly khoa hoc

--- teacherapp/urls.py
+++ teacherapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('lectures/create/', views.create_lecture, name='create_lecture'),
     path('lectures/', views.lecture_list, name='lecture_list'),
+    path('course/', views.lecture_list, name='course_list'),
+    path('course_management/', views.course_management, name='course_management'),
 ]
