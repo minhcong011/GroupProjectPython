bfd4fd66fed38f721428e0e3b4f5bc48e7ec6022
pkhoa011004 <164755043+pkhoa011004@users.noreply.github.com>
2025-06-30 09:37:16 +0700

    ss

--- cv/urls.py
+++ cv/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'cv'
+
 urlpatterns = [
     path('cv/', views.cv_edit, name='cv_edit'),
     path('cvsv/', views.cvsv_edit, name='cvsv_edit'),  # Đường dẫn cho sinh viên
     path('cv/password/', views.password_2fa, name='password_2fa'),
- ]
+]
--- cv/views.py
+++ cv/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from core.models import Account
 
@@ -24,6 +24,22 @@ def cv_edit(request):
 def cvsv_edit(request):
     try:
         account = Account.objects.get(username=request.user.username)
+        
+        if request.method == 'POST':
+            # Cập nhật thông tin từ form
+            account.email = request.POST.get('email', account.email)
+            account.first_name = request.POST.get('first_name', account.first_name)
+            account.last_name = request.POST.get('last_name', account.last_name)
+            account.company = request.POST.get('address', account.company)  # Sử dụng trường company làm address
+            account.phone_number = request.POST.get('phone_number', account.phone_number)
+            account.save()
+            
+            # Thêm thông báo thành công (nếu có messages framework)
+            from django.contrib import messages
+            messages.success(request, 'Cập nhật thông tin thành công!')
+            
+            return redirect('cvsv_edit')  # Redirect về trang hiện tại
+        
         context = {
             'email': account.email or '',
             'first_name': account.first_name or '',
@@ -34,7 +50,15 @@ def cvsv_edit(request):
             'phone_number': account.phone_number or '',
         }
     except Account.DoesNotExist:
-        context = {}
+        context = {
+            'email': '',
+            'first_name': '',
+            'last_name': '',
+            'address': '',
+            'title': '',
+            'timezone': '',
+            'phone_number': '',
+        }
 
     return render(request, 'cv/cvsv.html', context)
 
