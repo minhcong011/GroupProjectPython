1f0ea1b050910f961161dac5f084b5f6db601ce1
minhcong011 <dongminhcinh@gmail.com>
2025-06-30 10:03:25 +0700

    ss

--- teacherapp/urls.py
+++ teacherapp/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
 =======
     path('lectures/create/', views.create_lecture, name='create_lecture'),
     path('lectures/', views.lecture_list, name='lecture_list'),
-   
     path('course_management/', views.course_management, name='course_management'),
 
     path('cv/', cv_views.cv_edit, name='cv_edit'),
