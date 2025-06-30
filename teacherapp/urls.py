816dc9e8f4858b918fd7e9fa77b50a81041c6efe
minhcong011 <dongminhcinh@gmail.com>
2025-06-30 09:30:43 +0700

    ss

--- teacherapp/urls.py
+++ teacherapp/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from . import views as teacher_views
+from . import views
 from cv import views as cv_views
 
 urlpatterns = [
-    path('lectures/create/', teacher_views.create_lecture, name='create_lecture'),
-    path('lectures/', teacher_views.lecture_list, name='lecture_list'),
+    path('lectures/create/', views.create_lecture, name='create_lecture'),
+    path('lectures/', views.lecture_list, name='lecture_list'),
    
-    path('course_management/', teacher_views.course_management, name='course_management'),
+    path('course_management/', views.course_management, name='course_management'),
 
- 
     path('cv/', cv_views.cv_edit, name='cv_edit'),
     path('cv/password/', cv_views.password_2fa, name='password_2fa'),
 
